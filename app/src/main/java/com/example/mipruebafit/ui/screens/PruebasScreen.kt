8bc88712329aef218ce999371680c4c32fab70ac
Cristian20305 <cristianpenalbagar@gmail.com>
2025-02-25 19:31:07 +0100

    Buscador añadido en las pruebas funciona correctamente

--- app/src/main/java/com/example/mipruebafit/ui/screens/PruebasScreen.kt
+++ app/src/main/java/com/example/mipruebafit/ui/screens/PruebasScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.mipruebafit.Data.model.Prueba
@@ -19,6 +20,13 @@ fun PruebasScreen(pruebaSelected: (String) -> Unit, edadUsuario: Int) {
     // Lista de pruebas que se mostraran en la pantalla depende de la edad
     val pruebas = PruebasRepository.obtenerPruebasPorEdad(edadUsuario)
 
+    // Estado para el texto del buscador
+    var searchView by rememberSaveable { mutableStateOf("") }
+    // Lista de las pruebas filtradas por el texto
+    val pruebasFiltradas = rememberSaveable(searchView) {
+        pruebas.filter { it.nombre.contains(searchView, ignoreCase = true) }
+    }
+
     // Organizmao de manera vertical
     Column(
         modifier = Modifier
@@ -27,6 +35,18 @@ fun PruebasScreen(pruebaSelected: (String) -> Unit, edadUsuario: Int) {
     ) {
 
         Spacer(modifier = Modifier.height(50.dp))
+
+        // BUSCADOR
+        OutlinedTextField(
+            value = searchView,
+            // Se actualiza el texto del buscador
+            onValueChange = { searchView = it },
+            label = { Text("Buscar pruebas") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         Text(
             text = "Lista de Pruebas para esta edad $edadUsuario años",
@@ -37,7 +57,7 @@ fun PruebasScreen(pruebaSelected: (String) -> Unit, edadUsuario: Int) {
 
         LazyColumn {
             // Iteramos sobre cada uno para que nos lo muestre desde una lista
-            items(pruebas) { prueba ->
+            items(pruebasFiltradas) { prueba ->
                 PruebaItem(prueba, pruebaSelected)
             }
         }
